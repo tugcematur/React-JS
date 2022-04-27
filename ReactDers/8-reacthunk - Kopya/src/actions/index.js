f37f952805c6d578c7874bb07f04ba4d89e991b0
tugcematur <39506647+tugcematur@users.noreply.github.com>
2022-04-27 12:25:21 +0300

    update

--- ReactDers/8-reacthunk - Kopya/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import jsonPlaceHolder from '../apis/jsonPlaceHolder' 
-
-export const getBlogs = ()  => async dispatch => {
-    const response = await jsonPlaceHolder.get('/posts');
-    dispatch({type:'GET_BLOGS' ,payload: response.data})
-}
-     
-   
- 
